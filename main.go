67f23fb03758cdfe39a9627c258aab8ad0ea8a3d
Zheng Piaodan <zhengpiaodan@gmail.com>
2023-05-08 14:30:50 +0800

    fix: skip installing if package already installed

--- main.go
+++ main.go
@@ -22,6 +22,34 @@ func build_pkg_path(name string) string {
 	return fmt.Sprintf("legacyPackages.%s.%s", platform, name)
 }
 
+func get_local_pkgs() []string {
+	output_bytes, err := exec.Command("nix", "profile", "list").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	output_lines := strings.Split(strings.TrimRight(string(output_bytes[:]), "\n"), "\n")
+	var names []string
+	for _, line := range output_lines {
+		pkg_path := strings.Split(line, " ")[1]
+		splits := strings.Split(pkg_path, ".")
+		pkg_name := splits[len(splits)-1]
+		names = append(names, pkg_name)
+	}
+
+	return names
+}
+
+func already_installed(name string) bool {
+	for _, item := range get_local_pkgs() {
+		if item == name {
+			return true
+		}
+	}
+
+	return false
+}
+
 func main() {
 	app := &cli.App{
 		EnableBashCompletion: true,
@@ -31,8 +59,15 @@ func main() {
 				Aliases: []string{"i"},
 				Usage:   "install package from nixpkgs to nix default profile",
 				Action: func(cCtx *cli.Context) error {
-					pkg := "nixpkgs#" + cCtx.Args().First()
-					fmt.Println("installing", pkg)
+					name := cCtx.Args().First()
+
+					if already_installed(name) {
+						fmt.Println("[Skipped]", name, "is already installed.")
+						return nil
+					}
+
+					fmt.Println("installing", name)
+					pkg := "nixpkgs#" + name
 					cmd := exec.Command("nix", "profile", "install", pkg)
 					cmd.Stdout = os.Stdout
 					if err := cmd.Run(); err != nil {
@@ -46,19 +81,7 @@ func main() {
 				Aliases: []string{"l"},
 				Usage:   "list installed packages in nix default profile",
 				Action: func(cCtx *cli.Context) error {
-					output_bytes, err := exec.Command("nix", "profile", "list").Output()
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					output_lines := strings.Split(strings.TrimRight(string(output_bytes[:]), "\n"), "\n")
-					var names []string
-					for _, line := range output_lines {
-						pkg_path := strings.Split(line, " ")[1]
-						splits := strings.Split(pkg_path, ".")
-						pkg_name := splits[len(splits)-1]
-						names = append(names, pkg_name)
-					}
+					names := get_local_pkgs()
 
 					sort.Slice(names, func(x, y int) bool {
 						return names[x] < names[y]
